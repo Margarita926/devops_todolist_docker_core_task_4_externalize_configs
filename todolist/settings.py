08d0072f6e6bc10350ba61abc7cafb7b0c2345bb
Margaret <margarita@medentee.com>
2026-06-28 12:28:30 +0300

    add requested changes

--- todolist/settings.py
+++ todolist/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('NAME', BASE_DIR / 'db.sqlite3'),
+        'NAME': os.environ.get('NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
         'USER': os.environ.get('USER', ''),
         'PASSWORD': os.environ.get('PASSWORD', ''),
         'HOST': os.environ.get('HOST', ''),
